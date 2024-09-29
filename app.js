fba50a95f2a88e729844a307a96e898e5e042483
Om <130749526+Om7035@users.noreply.github.com>
2024-09-29 23:48:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,18 +5,18 @@ const PDFDocument = require("pdfkit");
 const fs = require("fs");
 const fsPromises = fs.promises;
 const path = require("path");
-const cors = require("cors"); // Add this line
+const cors = require("cors");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Configure CORS
-app.use(cors()); // Add this line
+app.use(cors());
+app.use(express.json({ limit: "10mb" }));
 
 // Configure multer
 const upload = multer({ dest: "upload/" });
-app.use(express.json({ limit: "10mb" }));
 
 // Initialize Google Generative AI
 const genAI = new GoogleGenerativeAI("AIzaSyBB79MwndfkzWwlYLjXg9529SYow9oNFA0");
